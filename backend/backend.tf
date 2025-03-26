3b1ae6d22800e0b0999c93e846cad34e35911e48
JungMin <ljlm0829@gmail.com>
2025-03-26 14:29:06 +0900

    skip policy

--- backend/backend.tf
+++ backend/backend.tf
@@ -23,38 +23,38 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.terraform_state.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+# resource "aws_s3_bucket_public_access_block" "public_access" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
 
-resource "aws_s3_bucket_policy" "terraform_state_policy" {
-  bucket = aws_s3_bucket.terraform_state.id
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": [
-        "s3:GetBucketPolicy",
-        "s3:ListBucket",
-        "s3:GetObject",
-        "s3:PutObject"
-      ],
-      "Resource": [
-        "arn:aws:s3:::marcello-terraformstate",
-        "arn:aws:s3:::marcello-terraformstate/*"
-      ]
-    }
-  ]
-}
-POLICY
-}
+# resource "aws_s3_bucket_policy" "terraform_state_policy" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": "*",
+#       "Action": [
+#         "s3:GetBucketPolicy",
+#         "s3:ListBucket",
+#         "s3:GetObject",
+#         "s3:PutObject"
+#       ],
+#       "Resource": [
+#         "arn:aws:s3:::marcello-terraformstate",
+#         "arn:aws:s3:::marcello-terraformstate/*"
+#       ]
+#     }
+#   ]
+# }
+# POLICY
+# }
 
 resource "aws_dynamodb_table" "terraform_lock" {
   name         = "marcello-terraformstate"
